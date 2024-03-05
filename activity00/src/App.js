78c4f50b8ad4bfed8c8657385b87dff424d1183a
Gabriel Al-Samir Guimaraes Sales <alsamir.gabriel@gmail.com>
2024-03-05 18:55:38 -0300

    feat(Atividade00): :tada: finish atv00 class02

--- activity00/src/App.js
+++ activity00/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import MyData1 from './components/atividade00/01MeusDados';
+import MyData2 from './components/atividade00/02MeusDados';
+import Temperature from './components/atividade00/03Temperatura';
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyData1/>
+      <div style={{borderTop: '3px solid #bbb'}}/>
+      <MyData2
+        name="José Vinícius Evangelista Dias de Souza"
+        course="Software Engineering"
+        university="UFC"
+      />
+      <div style={{borderTop: '3px solid #bbb'}}/>
+      <Temperature k={373} c={100} f={212}/>
     </div>
   );
 }
