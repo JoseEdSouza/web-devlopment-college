530cdd14709c591601d1794dfae00986b1ae1510
José Souza <joseedsouza.dev@gmail.com>
2024-09-17 17:00:13 -0300

    add cors

--- test02/back/server/app.py
+++ test02/back/server/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from server.domain.student.controller import student_router
 
 app = FastAPI()
+
+# config cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(student_router, prefix="/student")
